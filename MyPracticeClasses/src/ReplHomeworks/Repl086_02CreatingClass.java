a61bc861ff3b74c867edce57c84f5f574fcbf6c2
JasoorKarwar <karwar.jasoor@gmail.com>
2020-11-05 00:24:50 -0500

    "Class updated"

---
+++ MyPracticeClasses/src/ReplHomeworks/Repl086_02CreatingClass.java
@@ -0,0 +1,12 @@
+package ReplHomeworks;
+
+public class Repl086_02CreatingClass {
+    public static void main(String[] args){
+
+        String name="John";
+        int roll_num=2;
+
+        System.out.println("Name is "+name+ " and roll number is "+roll_num);
+
+    }
+}
---
+++ MyPracticeClasses/src/ReplHomeworks/Repl087_02ClassAndObjects.java
@@ -0,0 +1,19 @@
+package ReplHomeworks;
+
+public class Repl087_02ClassAndObjects {
+    public static void main(String[] args) {
+
+
+        String car1Color = "Black";
+        int car1Year = 2019;
+        String car1Make = "BMW";
+
+        String car2Color = "White";
+        int car2Year = 2018;
+        String car2Make = "Toyota";
+
+        System.out.println("Car color is"+" "+car1Color +" "+"and car year is"+" "+ car1Year + " " + "and car model is" +" " + car1Make);
+        System.out.println("Car color is"+" "+car2Color +" "+"and car year is"+" "+ car2Year + " " + "and car model is" +" " + car2Make);
+
+    }
+}
